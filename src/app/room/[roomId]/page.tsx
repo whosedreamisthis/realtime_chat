76561c0f3a518d75f20e451aaeb3fab502381fc4
Dana Sharon <whosedreamisthis@gmail.com>
2026-05-23 19:11:28 -0700

    add messaging to the room page, with controlled input

--- src/app/room/[roomId]/page.tsx
+++ src/app/room/[roomId]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { useParams } from "next/navigation";
 
 function formatTimeRemaining(seconds: number): string {
@@ -13,6 +13,8 @@ const RoomPage = () => {
   const roomId = params.roomId as string;
   const [copyStatus, setCopyStatus] = useState("COPY");
   const [timeRemaining, setTimeRemaining] = useState<number | null>(null);
+  const [input, setInput] = useState("");
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const copyLink = () => {
     const url = window.location.href;
@@ -57,6 +59,33 @@ const RoomPage = () => {
         </button>
       </header>
       <div className="flex-1 overflow-y-auto p-4 space-y-4 scrollbar-thin"></div>
+      <div className="p-4 border-t border-zinc-800 bg-zinc-900/30">
+        <div className="flex gap-4">
+          <div className="flex-1 relative group">
+            <span className="absolute left-4 top-1/2 -translate-y-1/2 text-green-500 animate-pulse">
+              {"> "}
+            </span>
+            <input
+              value={input}
+              ref={inputRef}
+              placeholder="Type your message here"
+              onKeyDown={(e) => {
+                if (e.key === "Enter" && input.trim()) {
+                  // TODO: SEND MESSAGE
+                  inputRef.current?.focus();
+                }
+              }}
+              onChange={(e) => setInput(e.target.value)}
+              autoFocus
+              type="text"
+              className="w-full bg-black border border-zinc-800 focus:border-zinc-700 focus:outline-none transition-colors text-zinc-100 placeholder:text-zinc-700 py-3 pl-8 pr-4 text-sm"
+            />
+          </div>
+          <button className="bg-zinc-800 text-zinc-400 px-6 text-sm font-bold hover:text-zinc-200 transition-all disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer">
+            SEND
+          </button>
+        </div>
+      </div>
     </main>
   );
 };
